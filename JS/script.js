c989ebb416b9fedbdc6a5948bbeeb080e76abe71
mohamed <mohamedzerouali203@gmail.com>
2022-08-09 14:03:57 +0100

    nothing realy

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,8 @@
 let bars = document.querySelector(".bars");
 let links = document.querySelector(".link-cont")
+
 console.log(bars)
+
 bars.addEventListener("click",()=>{
     links.classList.toggle("open")
 })
